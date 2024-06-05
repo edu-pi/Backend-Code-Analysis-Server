0f236e21e6363d0123cf8dc63e41acfc416470bd
UJeans <jean207@naver.com>
2024-06-05 12:21:08 +0900

    [HAE-25]feat: for문 파싱 코드 옮겨오기

--- app/analysis/parser.py
+++ app/analysis/parser.py
@@ -2,14 +2,14 @@ import ast
 import re
 
 from app.analysis.element_manager import CodeElementManager
-from app.analysis.models import Variable
+from app.analysis.models import *
 
 g_elem_manager = CodeElementManager()
-
+print("CodeElementManager 생성")
 
 def assign_parse(node):
     for target in node.targets:
-        depth = g_elem_manager.g_depth
+        depth = g_elem_manager.depth
         if isinstance(node.value, ast.BinOp):
             parsed_expressions = binOp_parse(node.value)
             for parsed_expression in parsed_expressions:
@@ -91,3 +91,77 @@ def constant_parse(node):
 
 def for_parse(node):
     target_name = node.target.id
+    # 타겟 처리
+    target_name = node.target.id
+
+    # Condition 객체 생성
+    if isinstance(node.iter, ast.Call):
+        condition = create_condition(target_name, node.iter)
+
+    # For 객체 생성
+    g_elem_manager.addStep(
+        For(id=id(condition), depth= g_elem_manager.depth, condition=condition)
+    )
+
+    # Body 처리
+    g_elem_manager.increase_depth()
+    for i in range(condition.start, condition.end, condition.step):
+        for child_node in node.body:
+            if isinstance(child_node, ast.Expr):
+                # target 업데이트
+                g_elem_manager.add_variable_value(name=target_name, value=i)
+                expr_parse(child_node, target_name)
+
+        # condition 객체에서 cur 값만 변경한 새로운 condition 생성
+        new_condition = condition.copy_with_new_cur(i)
+        g_elem_manager.addStep(
+            For(id=id(condition), depth=g_elem_manager.get_depth() - 1, condition=new_condition)
+        )
+
+
+def expr_parse(node, target_name):
+    # TODO(taget_name을 넘기면 안될 것 같음 but, node 삽입엔 target_name이 필요)
+    for cur_node in node.value.args:
+        if isinstance(cur_node, ast.BinOp):
+            result = binOp_calculate_binOp(cur_node, ast.unparse(cur_node))
+            # 연산 과정 리스트 생성
+            parsed_expressions = binOp_parse(cur_node)
+            # 중간 연산 과정이 포함된 노드 생성
+            for parsed_expression in parsed_expressions:
+                g_elem_manager.addStep(
+                    Variable(depth=g_elem_manager.get_depth(), name=target_name, expr=parsed_expression)
+                )
+
+
+def create_condition(target_name, node: ast.Call):
+    # range 파싱
+    if node.func.id == "range":
+        print("range 파싱")
+
+    # Condition - start, end, step
+    start = 0
+    end = None
+    step = 1
+
+    # args의 개수에 따라 start, end, step에 값을 할당
+    identifier_list = [identifier_parse(arg) for arg in node.args]
+
+    if len(identifier_list) == 1:
+        end = identifier_list[0]
+    elif len(identifier_list) == 2:
+        start, end = identifier_list
+    elif len(identifier_list) == 3:
+        start, end, step = identifier_list
+
+    return Condition(name=target_name, start=start, end=end, step=step, cur=start)
+
+
+def identifier_parse(node):
+    if isinstance(node, ast.Name):
+        try:
+            return g_elem_manager.get_variable_value(name=node.id)
+        except NameError as e:
+            print("#error:", e)
+            return g_elem_manager.add_variable_value(name=node.id, value=0)
+    elif isinstance(node, ast.Constant):
+        return node.value
