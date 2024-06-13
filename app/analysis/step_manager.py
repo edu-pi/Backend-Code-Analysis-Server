e1eeb29b5dd17cb69569c5c472fe331d52f397e6
UJeans <jean207@naver.com>
2024-06-13 15:07:35 +0900

    [#57]rename: StepManager의 멤버 변수 이름 변겅, nodes -> viz_nodes

--- app/analysis/step_manager.py
+++ app/analysis/step_manager.py
@@ -1,13 +1,13 @@
 class StepManager:
 
     def __init__(self):
-        self.nodes = []
+        self.viz_nodes = []
 
     def add_step(self, elem):
-        self.nodes.append(elem)
+        self.viz_nodes.append(elem)
 
     def add_steps(self, elem_list: list):
-        self.nodes += elem_list
+        self.viz_nodes += elem_list
 
     def get_all_step(self):
-        return self.nodes
+        return self.viz_nodes
