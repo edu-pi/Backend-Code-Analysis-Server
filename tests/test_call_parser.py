5062c2a99111ceb51bc5690d96b29bb6f8c8ae11
UJeans <jean207@naver.com>
2024-06-14 11:44:28 +0900

    [#61]test: 함수 아이디 추출 함수의 성공과 실패 경우의 테스트 함수를 합침

--- tests/test_call_parser.py
+++ tests/test_call_parser.py
@@ -20,39 +20,27 @@ def call_parser(elem_manager):
     return _create_parser
 
 
-@pytest.mark.parametrize(
-    "node, expect",
-    [
-        (ast.Call(func=ast.Name(id='print', ctx=ast.Load()), args=[], keywords=[]), 'print'),
-        (ast.Call(func=ast.Name(id='range', ctx=ast.Load()), args=[], keywords=[]), 'range'),
-    ]
-)
-def test_get_func_name_success(call_parser, node, expect):
+# 테스트 데이터를 별도의 변수에 저장
+test_data = [
+    (ast.Call(func=ast.Name(id='print', ctx=ast.Load()), args=[], keywords=[]), 'print', True),
+    (ast.Call(func=ast.Name(id='range', ctx=ast.Load()), args=[], keywords=[]), 'range', True),
+    (ast.Call(func=ast.Name(id='print', ctx=ast.Load()), args=[], keywords=[]), '', False),
+    (ast.Call(func=ast.Name(id='range', ctx=ast.Load()), args=[], keywords=[]), 'print', False),
+]
+
+
+@pytest.mark.parametrize("node, expect, success", test_data)
+def test_get_func_name(call_parser, node, expect, success):
     """
     ast.Call 노드가 주어졌을 때 함수 아이디를 제대로 가져오는지 테스트
     :param node: ast.Call 노드
     :param expect: 예상되는 함수 아이디
+    :param success: 테스트가 성공해야 하는지 (True), 실패해야 하는지 (False)
     :return: None
     """
     parser = call_parser(node)
     result = parser._CallParser__get_func_name()
-    assert result == expect
-
-
-@pytest.mark.parametrize(
-    "node, expect",
-    [
-        (ast.Call(func=ast.Name(id='print', ctx=ast.Load()), args=[], keywords=[]), ''),
-        (ast.Call(func=ast.Name(id='range', ctx=ast.Load()), args=[], keywords=[]), 'print'),
-    ]
-)
-def test_get_func_name_failure(call_parser, node, expect):
-    """
-    ast.Call 노드가 주어졌을 때 함수 아이디를 제대로 가져오는지 테스트
-    :param node: ast.Call 노드
-    :param expect: 예상되는 함수 아이디
-    :return: None
-    """
-    parser = call_parser(node)
-    result = parser._CallParser__get_func_name()
-    assert result != expect
+    if success:
+        assert result == expect
+    else:
+        assert result != expect
