3794780b8605011e8237b1aa6ccfd7be052aa97f
GS_song <20003204@sju.ac.kr>
2024-06-15 19:27:26 +0900

    [#66]test: NameParser가 static으로 바뀌면서 test코드 변경

--- tests/analysis/generator/parser/test_name_parser.py
+++ tests/analysis/generator/parser/test_name_parser.py
@@ -12,40 +12,20 @@ def elem_manager():
     return mock
 
 
-@pytest.fixture
-def name_parser(elem_manager):
-    # NameParser 인스턴스를 생성하는 함수를 반환
-    def _create_parser(node):
-        return NameParser(node, elem_manager)
-
-    return _create_parser
-
-
-@pytest.fixture
-def create_ast_name_node():
-    # 코드를 ast.Name 노드로 변환하는 함수를 반환
-    def _create_ast_name_node(name_id, ctx):
-        # ast.Name 반환
-        return ast.Name(id=name_id, ctx=ctx)
-
-    return _create_ast_name_node
-
-
 @pytest.mark.parametrize("name_id, ctx, expect_value, expect_expressions", [
     ("abc", ast.Store(), None, None),
     ("b", ast.Load(), 10, ['b', '10'])
 ])
-def test_parse(create_ast_name_node, name_parser, name_id, ctx, expect_value, expect_expressions):
+def test_parse(elem_manager, name_id, ctx, expect_value, expect_expressions):
     """
     ast.Name 노드가 주어졌을 때 Name 인스턴스를 생성하는지 테스트
     """
-    name_node = create_ast_name_node(name_id, ctx)
+    name_node = ast.Name(id=name_id, ctx=ctx)
     expect = Name(name_id, expect_value, expect_expressions)
 
     with patch.object(NameParser, '_NameParser__get_value', return_value=expect_value), \
             patch.object(NameParser, '_NameParser__get_expressions', return_value=expect_expressions):
-        parser = name_parser(name_node)
-        result = parser.parse()
+        result = NameParser.parse(name_node, elem_manager)
 
         assert result == expect
 
@@ -55,13 +35,12 @@ def test_parse(create_ast_name_node, name_parser, name_id, ctx, expect_value, ex
     ("abc_abc", ast.Load(), 10),
     ("abcAbc", ast.Load(), 10)
 ])
-def test__get_value(create_ast_name_node, name_parser, name_id, ctx, expect):
+def test__get_value(elem_manager, name_id, ctx, expect):
     """
     ast.Name 노드가 주어졌을 때 value를 가져오는지 테스트
     """
-    name_node = create_ast_name_node(name_id, ctx)
-    parser = name_parser(name_node)
-    result = parser._NameParser__get_value()
+
+    result = NameParser._NameParser__get_value(elem_manager, name_id)
 
     assert result == expect
 
@@ -71,12 +50,10 @@ def test__get_value(create_ast_name_node, name_parser, name_id, ctx, expect):
     ("abc_abc", 10, ast.Load(), ['abc_abc', '10']),
     ("abcAbc", 10, ast.Load(), ['abcAbc', '10'])
 ])
-def test__get_expressions(create_ast_name_node, name_parser, name_id, value, ctx, expect):
+def test__get_expressions(name_id, value, ctx, expect):
     """
     ast.Name 노드가 주어졌을 때 value를 가져오는지 테스트
     """
-    name_node = create_ast_name_node(name_id, ctx)
-    parser = name_parser(name_node)
-    result = parser._NameParser__get_expressions(value)
+    result = NameParser._NameParser__get_expressions(name_id, value)
 
     assert result == expect
