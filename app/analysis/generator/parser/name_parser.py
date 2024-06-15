33578702af48cbf10ec50f275bb787f826b4924c
GS_song <20003204@sju.ac.kr>
2024-06-15 15:41:50 +0900

    [#32]feat: class 범위에서 필드값에 직접 접근을 명시적으로 제한

--- app/analysis/generator/parser/name_parser.py
+++ app/analysis/generator/parser/name_parser.py
@@ -7,34 +7,34 @@ from app.analysis.element_manager import CodeElementManager
 class NameParser:
 
     def __init__(self, node: ast.Name, elem_manager: CodeElementManager):
-        self.ctx = node.ctx
-        self.id = node.id
-        self.elem_manager = elem_manager
+        self.__ctx = node.ctx
+        self.__name_id = node.id
+        self.__elem_manager = elem_manager
 
     def parse(self):
-        if isinstance(self.ctx, ast.Store):
+        if isinstance(self.__ctx, ast.Store):
             value = None
             expressions = None
 
-        elif isinstance(self.ctx, ast.Load):
+        elif isinstance(self.__ctx, ast.Load):
             value = self.__get_value()
             expressions = self.__get_expressions(value)
 
         else:
-            raise NotImplementedError(f"Unsupported node type: {type(self.ctx)}")
+            raise NotImplementedError(f"Unsupported node type: {type(self.__ctx)}")
 
-        return Name(self.id, value, expressions)
+        return Name(self.__name_id, value, expressions)
 
     # 변수의 값을 가져오는 함수
     def __get_value(self):
         try:
-            return self.elem_manager.get_variable_value(name=self.id)
+            return self.__elem_manager.get_variable_value(name=self.__name_id)
         except NameError as e:
             print("#error:", e)
 
     # 변수의 변화 과정을 만들어주는 함수
     def __get_expressions(self, value):
-        return [self.id, str(value)]
+        return [self.__name_id, str(value)]
 
 
 @dataclass
