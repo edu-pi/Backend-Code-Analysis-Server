76f0928b9b4af05804c8aba87bf15d2b792856f2
GS_song <20003204@sju.ac.kr>
2024-06-18 14:47:30 +0900

    [#78]feat: parse에서 하는 _calculate_node의 결과 계산 로직을 합치기 위해 BinopParser의 init에 node자체를 저장하고 left, right,op를 꺼내서 사용하도록 수정

--- app/analysis/generator/parser/binop_parser.py
+++ app/analysis/generator/parser/binop_parser.py
@@ -13,31 +13,25 @@ class BinopParser:
 
     def __init__(self, node: ast.BinOp, elem_manager: CodeElementManager):
         self.__elem_manager = elem_manager
-        self.__left = node.left
-        self.__op = node.op
-        self.__right = node.right
-        self.__initial_expression = ast.unparse(node)
+        self.__node = node
 
     @staticmethod
     def parse(node: ast.BinOp, elem_manager: CodeElementManager):
-
         binop_parser = BinopParser(node, elem_manager)
 
-        # 왼쪽, 오른쪽 노드를 계산
-        left = binop_parser.__calculate_node(node.left)
-        right = binop_parser.__calculate_node(node.right)
-
         # 계산 결과를 연산자에 따라 계산
-        result = binop_parser.__calculate_value(left, right)
-        expressions = binop_parser.__create_expressions(result)
+        result = binop_parser.__calculate_node(node)
+        expressions = binop_parser.__create_expressions(result, ast.unparse(node))
 
         return Binop(result, expressions)
 
     # 연산식을 따라가면서 계산해 결과를 반환
-    def __calculate_node(self, node: ast):
+    def __calculate_node(self, node):
         if isinstance(node, ast.BinOp):
-            binop = BinopParser.parse(node, self.__elem_manager)
-            return binop.value
+            left = self.__calculate_node(node.left)
+            right = self.__calculate_node(node.right)
+
+            return self.__calculate_value(left, right, node.op)
 
         elif isinstance(node, ast.Name):
             name = NameParser.parse(node, self.__elem_manager)
@@ -51,41 +45,42 @@ class BinopParser:
             raise NotImplementedError(f"Unsupported node type: {type(node)}")
 
     # 왼쪽 오른쪽 값으로 연산식 계산
-    def __calculate_value(self, left: int, right: int):
-        if isinstance(self.__op, ast.Add):
+    def __calculate_value(self, left: int, right: int, op: ast):
+        if isinstance(op, ast.Add):
             return left + right
 
-        elif isinstance(self.__op, ast.Sub):
+        elif isinstance(op, ast.Sub):
             return left - right
 
-        elif isinstance(self.__op, ast.Mult):
+        elif isinstance(op, ast.Mult):
             return left * right
 
-        elif isinstance(self.__op, ast.Div):  # '/'
+        elif isinstance(op, ast.Div):  # '/'
             return left / right  # 실수로 계산
 
-        elif isinstance(self.__op, ast.FloorDiv):  # '//'
+        elif isinstance(op, ast.FloorDiv):  # '//'
             return left // right  # 정수로 계산
 
         else:
-            raise NotImplementedError(f"Unsupported operator: {type(self.__op)}")
+            raise NotImplementedError(f"Unsupported operator: {type(op)}")
 
-    def __create_expressions(self, result: int):
+    def __create_expressions(self, result: int, initial_expression: str):
         # 초기 계산식 저장
-        expression = self.__initial_expression
-        expressions = [expression]
+        expressions = [initial_expression]
         pattern = r'\b[a-zA-Z_]\w*\b'
 
         # 변수 이름 추출
-        target_names = set(re.findall(pattern, expression))
+        target_names = set(re.findall(pattern, initial_expression))
+        next_expression = initial_expression
 
         # 변수들을 값으로 대체
         for original_name in target_names:
             replace_value = self.__elem_manager.get_variable_value(original_name)
-            expression = replace_word(expression=expression, original_word=original_name, new_word=replace_value)
+            next_expression = replace_word(expression=next_expression, original_word=original_name,
+                                           new_word=replace_value)
 
         if len(target_names) != 0:
-            expressions.append(expression)
+            expressions.append(next_expression)
 
         # 마지막 계산 결과 저장
         expressions.append(str(result))
