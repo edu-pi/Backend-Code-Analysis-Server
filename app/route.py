d7abbdbe24b047dbeb1d8a8a4ba802b8ab723c8b
GS_song <20003204@sju.ac.kr>
2024-06-19 12:04:00 +0900

    [#36]feat: front와 연결하기 위해 cors 에러를 해결, 입력받는 request 데이터 정의

--- app/route.py
+++ app/route.py
@@ -1,19 +1,42 @@
 import ast
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 from app.analysis.code_analyzer import CodeAnalyzer
 from app.analysis.element_manager import CodeElementManager
 
 app = FastAPI()
 
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+class RequestCode(BaseModel):
+    source_code: str
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
 
 @app.post("/v1/python")
-def read_root(source_code):
+def read_root(request_code: RequestCode):
     # 코드 분석 인스턴스 생성
     code_analyzer = CodeAnalyzer(CodeElementManager())
 
     # 소스 코드를 추상 구문 트리로 변환
-    parsed_ast = ast.parse(source_code)
+    parsed_ast = ast.parse(request_code.source_code)
 
     return code_analyzer.visualize_code(parsed_ast)
