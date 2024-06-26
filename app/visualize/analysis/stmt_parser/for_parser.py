b4d9c99f8a3356723df3da5ea22eaed66998a317
UJeans <jean207@naver.com>
2024-06-26 14:51:01 +0900

    [#89]rename: _get_condition_value_list -> _get_condition_obj

--- app/visualize/analysis/stmt_parser/for_parser.py
+++ app/visualize/analysis/stmt_parser/for_parser.py
@@ -8,7 +8,7 @@ class ForParser:
     @staticmethod
     def parse(target: ast, iter: ast, elem_manager: CodeElementManager):
         target_name = ForParser._get_target_name(target, elem_manager)
-        condition_dict = ForParser._get_condition_value_list(iter, elem_manager)
+        condition_dict = ForParser._get_condition_obj(iter, elem_manager)
 
         return {"target": target_name, "condition_dict": condition_dict}
 
@@ -21,11 +21,11 @@ class ForParser:
             raise TypeError(f"[ForParser]:  {type(target)}는 잘못된 타입입니다.")
 
     @staticmethod
-    def _get_condition_value_list(iter: ast, elem_manager: CodeElementManager):
+    def _get_condition_obj(iter: ast, elem_manager: CodeElementManager):
         if isinstance(iter, ast.Call):
             # {value: {start: 3, end: 10, step: 2}, expressions: [{start: a, end: 10, step: 2}...{start: 3, end: 10, step: 2}]
-            return ExprTraveler.call_travel(iter, elem_manager)
-
+            range_obj = ExprTraveler.call_travel(iter, elem_manager)
+            return range_obj
         elif isinstance(iter, ast.List):
             return
 
---
+++ tests/visualize/analysis/stmt_parser/expr_analysis/test_for_parser.py
@@ -0,0 +1,44 @@
+import ast
+from unittest.mock import MagicMock, patch
+
+from app.visualize.analysis.element_manager import CodeElementManager
+from app.visualize.analysis.stmt_parser.expr_analysis.expr_parser.call_parser import Range, CallParser
+import pytest
+
+from app.visualize.analysis.stmt_parser.expr_analysis.expr_traveler import ExprTraveler
+from app.visualize.analysis.stmt_parser.for_parser import ForParser
+
+
+@pytest.fixture
+def create_ast():
+    # 코드를 ast 노드로 변환하는 함수를 반환
+    def _create_ast_node(code):
+        # ast.Call 반환
+        return ast.parse(code).body[0]
+
+    return _create_ast_node
+
+
+@pytest.fixture
+def elem_manager():
+    mock = MagicMock(spec=CodeElementManager)
+    return mock
+
+
+## TODO 함수마다 parser를 추가하는 방향 고민
+@pytest.mark.parametrize("code, expect", [
+    (
+            '''for i in range(3): \n    pass''',
+            Range(value={'end': '3', 'start': '0', 'step': '1'},
+                  expressions=[{'end': '3', 'start': '0', 'step': '1'}])
+    )
+])
+def test_get_condition_value_list(create_ast, elem_manager, code, expect):
+    # iter 노드를 받아서 range 함수의 파라미터를 반환하는지 테스트
+    iter_node = create_ast(code).iter
+
+    with patch.object(ExprTraveler, 'call_travel',
+                      return_value=Range(value={'end': '3', 'start': '0', 'step': '1'},
+                                         expressions=[{'end': '3', 'start': '0', 'step': '1'}])):
+        actual = ForParser._get_condition_obj(iter_node, elem_manager)
+        assert actual == expect
