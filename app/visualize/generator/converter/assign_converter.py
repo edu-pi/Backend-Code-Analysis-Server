0d91bdf9a8750f6b2acb2fd158c9199a421e3cf5
GS_song98 <20003204@sju.ac.kr>
2024-07-01 11:12:10 +0900

    [#105]fix: assign_viz를 만드는 방식 수정

--- app/visualize/generator/converter/assign_converter.py
+++ app/visualize/generator/converter/assign_converter.py
@@ -6,12 +6,15 @@ from app.visualize.generator.model.models import AssignViz, Variable
 class AssignConverter:
     @staticmethod
     def convert(assign_obj: AssignStmtObj):
-        variable_list = []
+        assign_viz_list = []
         highlights = highlight.expressions_highlight_indices(assign_obj.expressions)
 
         for expr_idx, expression in enumerate(assign_obj.expressions):
+            variable_list = []
 
             for target in assign_obj.targets:
+                # Todo depth 추가
                 variable_list.append(Variable(name=target, expr=expression, highlights=highlights[expr_idx], depth=1))
+            assign_viz_list.append(AssignViz(variables=variable_list))
 
-        return AssignViz(variables=variable_list)
+        return assign_viz_list
--- app/visualize/generator/converter_traveler.py
+++ app/visualize/generator/converter_traveler.py
@@ -8,7 +8,7 @@ class ConverterTraveler:
         viz_objs = []
         for analysis_obj in analysis_objs:
             if analysis_obj.type == "assign":
-                viz_objs.append(AssignConverter.convert(analysis_obj))
+                viz_objs.extend(AssignConverter.convert(analysis_obj))
 
             else:
                 raise TypeError(f"지원하지 않는 노드 타입입니다.: {analysis_obj.type}")
