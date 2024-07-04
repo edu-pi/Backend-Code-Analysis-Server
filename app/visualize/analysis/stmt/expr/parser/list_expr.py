711d96000e90d50088970673cdcfa2730b1f98e8
GS_song98 <20003204@sju.ac.kr>
2024-07-04 13:41:50 +0900

    [#39]feat: 문자열 만드는 과정에 대한 예시 주석 추가

--- app/visualize/analysis/stmt/expr/parser/list_expr.py
+++ app/visualize/analysis/stmt/expr/parser/list_expr.py
@@ -1,20 +1,20 @@
-import ast
+from typing import List
 
 from app.visualize.analysis.stmt.expr.expr_util import util
-from app.visualize.analysis.stmt.expr.model.expr_obj import ListObj
+from app.visualize.analysis.stmt.expr.model.expr_obj import ListObj, ExprObj
 
 
 class ListExpr:
 
     @staticmethod
-    def parse(elts):
+    def parse(elts: List[ExprObj]):
         value = ListExpr._get_value(elts)
         expressions = ListExpr._concat_expression(elts)
 
         return ListObj(value=value, expressions=expressions)
 
     @staticmethod
-    def _get_value(elts):
+    def _get_value(elts: List[ExprObj]):
         return tuple(elt.value for elt in elts)
 
     @staticmethod
@@ -22,4 +22,5 @@ class ListExpr:
         elts_lists = [elt.expressions for elt in elts]
         transposed_lists = util.transpose_with_last_fill(elts_lists)
 
+        # [[1, 2, 3], [4, 5, 6]] -> ("[1, 4]", "[2, 5]", "[3, 6]")
         return tuple(f"[{','.join(map(str, t))}]" for t in transposed_lists)
