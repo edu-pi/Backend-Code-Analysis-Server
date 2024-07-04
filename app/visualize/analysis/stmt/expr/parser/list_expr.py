d10506b9707df297813afd6511375708124ae594
GS_song98 <20003204@sju.ac.kr>
2024-07-04 13:46:44 +0900

    [#39]feat: ListExpr에 들어오는 매개변수 타입 힌트 제공

--- app/visualize/analysis/stmt/expr/parser/list_expr.py
+++ app/visualize/analysis/stmt/expr/parser/list_expr.py
@@ -7,20 +7,20 @@ from app.visualize.analysis.stmt.expr.model.expr_obj import ListObj, ExprObj
 class ListExpr:
 
     @staticmethod
-    def parse(elts: List[ExprObj]):
+    def parse(elts: list[ExprObj]):
         value = ListExpr._get_value(elts)
         expressions = ListExpr._concat_expression(elts)
 
         return ListObj(value=value, expressions=expressions)
 
     @staticmethod
-    def _get_value(elts: List[ExprObj]):
+    def _get_value(elts: list[ExprObj]):
         return tuple(elt.value for elt in elts)
 
     @staticmethod
-    def _concat_expression(elts):
-        elts_lists = [elt.expressions for elt in elts]
-        transposed_lists = util.transpose_with_last_fill(elts_lists)
+    def _concat_expression(elts: list[ExprObj]):
+        elts_expression_lists = [elt.expressions for elt in elts]
+        transposed_expression_lists = util.transpose_with_last_fill(elts_expression_lists)
 
         # [[1, 2, 3], [4, 5, 6]] -> ("[1, 4]", "[2, 5]", "[3, 6]")
-        return tuple(f"[{','.join(map(str, t))}]" for t in transposed_lists)
+        return tuple(f"[{','.join(map(str, t))}]" for t in transposed_expression_lists)
