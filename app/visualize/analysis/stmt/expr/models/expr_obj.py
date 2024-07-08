a94e4be1d4246290e0159fa5d151134a671caa70
GS_song98 <20003204@sju.ac.kr>
2024-07-08 17:38:07 +0900

    [#60]style: printObj 중복 제거, ExprObj 상속 필드 제거

--- app/visualize/analysis/stmt/expr/models/expr_obj.py
+++ app/visualize/analysis/stmt/expr/models/expr_obj.py
@@ -7,49 +7,32 @@ from app.visualize.analysis.stmt.expr.models.range_expression import RangeExpres
 @dataclass(frozen=True)
 class ExprObj:
     value: Any
-    expressions: tuple[Any, ...]
+    expressions: tuple[str, ...]
     type: str
 
 
 @dataclass(frozen=True)
-class PrintObj(ExprObj):
-    value: str
-    expressions: tuple[str, ...]
-    type: str = field(default="print", init=False)
-
-
-@dataclass(frozen=True)
 class BinopObj(ExprObj):
-    value: Any
-    expressions: tuple[str, ...]
     type: str = field(default="binop", init=False)
 
 
 @dataclass(frozen=True)
 class CompareObj(ExprObj):
-    value: Any
-    expressions: tuple[str, ...]
     type: str = field(default="compare", init=False)
 
 
 @dataclass(frozen=True)
 class CallObj(ExprObj):
-    value: Any
-    expressions: tuple[str, ...]
     type: str = field(default="call", init=False)
 
 
 @dataclass(frozen=True)
 class ConstantObj(ExprObj):
-    value: Any
-    expressions: tuple[str, ...]
     type: str = field(default="constant", init=False)
 
 
 @dataclass(frozen=True)
 class NameObj(ExprObj):
-    value: Any
-    expressions: tuple[str, ...]
     type: str = field(default="name", init=False)
 
 
@@ -70,5 +53,4 @@ class RangeObj(ExprObj):
 @dataclass(frozen=True)
 class PrintObj(ExprObj):
     value: str
-    expressions: tuple[str, ...]
     type: str = field(default="print", init=False)
