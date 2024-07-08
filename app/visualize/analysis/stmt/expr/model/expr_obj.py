842ad15b13952d5767c94d919c1bcf0917e22b54
GS_song98 <20003204@sju.ac.kr>
2024-07-08 15:51:29 +0900

    [#137]fix: 형식에 맞게 위치 수정

--- app/visualize/analysis/stmt/expr/model/expr_obj.py
+++ app/visualize/analysis/stmt/expr/model/expr_obj.py
@@ -6,9 +6,9 @@ from app.visualize.analysis.stmt.expr.model.range_expression import RangeExpress
 
 @dataclass(frozen=True)
 class ExprObj:
-    type: str
     value: Any
     expressions: tuple[Any, ...]
+    type: str
 
 
 @dataclass(frozen=True)
