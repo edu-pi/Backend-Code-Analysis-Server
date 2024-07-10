c631e50c39407461220780592c66e00f07e6f8e7
GS_song98 <20003204@sju.ac.kr>
2024-07-10 10:58:14 +0900

    [#149]test: ListExpr.parse()함수에 대한 mock 테스트를 작성, 하위 private 함수에 대한 테스트 추가

--- tests/visualize/analysis/stmt/parser/expr/parser/test_list_expr.py
+++ tests/visualize/analysis/stmt/parser/expr/parser/test_list_expr.py
@@ -1,40 +1,90 @@
-# _*_ coding: utf-8 _*_
-
 import pytest
 
-from app.visualize.analysis.stmt.parser.expr.models.expr_obj import ListObj, ConstantObj, BinopObj, NameObj, ExprObj
+from app.visualize.analysis.stmt.parser.expr.models.expr_obj import ListObj, ExprObj
 from app.visualize.analysis.stmt.parser.expr.parser.list_expr import ListExpr
 
 
 @pytest.mark.parametrize(
-    "elts, expected",
+    "value, expressions",
     [
+        pytest.param([10, 20], ("[10,20]",), id="[10, 20]: success case"),
         pytest.param(
-            [ConstantObj(value=10, expressions=("10",)), ConstantObj(value=20, expressions=("20",))],
-            ListObj(value=[10, 20], expressions=("[10,20]",)),
-            id="[10, 20]: success case",
-        ),
-        pytest.param(
-            [BinopObj(value=11, expressions=("a + 1", "10 + 1", "11")), ConstantObj(value=20, expressions=("20",))],
-            ListObj(value=[11, 20], expressions=("[a + 1,20]", "[10 + 1,20]", "[11,20]")),
+            [11, 20],
+            ("[a + 1,20]", "[10 + 1,20]", "[11,20]"),
             id="[a + 1, 20]: success case",
         ),
         pytest.param(
-            [
-                ConstantObj(value="Hello", expressions=("'Hello'",)),
-                ConstantObj(value="World", expressions=("'World'",)),
-            ],
-            ListObj(value=["Hello", "World"], expressions=("['Hello','World']",)),
+            ["Hello", "World"],
+            ("['Hello','World']",),
             id='["Hello", "World"]: success case',
         ),
         pytest.param(
-            [NameObj(value="a", expressions=("a",)), NameObj(value="b", expressions=("b",))],
-            ListObj(value=["a", "b"], expressions=("[a,b]",)),
+            ["a", "b"],
+            ("[a,b]",),
             id="[a, b] success case",
         ),
     ],
 )
-def test_parse(elts: list[ExprObj], expected):
-    result = ListExpr.parse(elts)
+def test_parse(mocker, value, expressions):
+    # _get_value mocking
+    mocker.patch("app.visualize.analysis.stmt.parser.expr.parser.list_expr.ListExpr._get_value", return_value=value)
+    # _concat_expression mocking
+    mocker.patch(
+        "app.visualize.analysis.stmt.parser.expr.parser.list_expr.ListExpr._concat_expression",
+        return_value=expressions,
+    )
+
+    result = ListExpr.parse([mocker.MagicMock(spec=ExprObj) for _ in range(len(value))])
+
+    assert isinstance(result, ListObj)
+    assert result.value == value
+    assert result.expressions == expressions
+
+
+@pytest.mark.parametrize(
+    "value",
+    [
+        pytest.param([10, 20], id="[10, 20]: success case"),
+        pytest.param([11, 20], id="[11, 20]: success case"),
+        pytest.param(["Hello", "World"], id='["Hello", "World"]: success case'),
+        pytest.param(["a", "b"], id="[a, b] success case"),
+    ],
+)
+def test_get_value(mocker, value):
+    # elts 생성
+    elts = [mocker.MagicMock(spec=ExprObj, value=v) for v in value]
+    result = ListExpr._get_value(elts)
+
+    assert isinstance(result, list)
+    assert result == value
+
+
+@pytest.mark.parametrize(
+    "expressions, transposed_expression_lists_result, expected",
+    [
+        pytest.param([("10",), ("20",)], [("10", "20")], ("[10,20]",), id="[10, 20]: success case"),
+        pytest.param(
+            [("a", "10"), ("20",), ("30",)],
+            [("a", "20", "30"), ("10", "20", "30")],
+            ("[a,20,30]", "[10,20,30]"),
+            id="[a, 20, 30]: success case",
+        ),
+        pytest.param(
+            [("a + 1", "10 + 1", "11"), ("20",)],
+            [("a + 1", "20"), ("10 + 1", "20"), ("11", "20")],
+            ("[a + 1,20]", "[10 + 1,20]", "[11,20]"),
+            id="[a + 1, 20]: success case",
+        ),
+    ],
+)
+def test_concat_expression(mocker, expressions, transposed_expression_lists_result, expected):
+    # elts 생성
+    elts = [mocker.MagicMock(spec=ExprObj, expressions=expr) for expr in expressions]
+    mocker.patch(
+        "app.visualize.utils.utils.transpose_with_last_fill",
+        return_value=transposed_expression_lists_result,
+    )
+
+    result = ListExpr._concat_expression(elts)
 
     assert result == expected
