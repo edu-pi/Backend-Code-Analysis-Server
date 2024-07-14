efc85393a1df821501ee499f71e7abbf260e7a6f
GS_song98 <20003204@sju.ac.kr>
2024-07-14 19:24:18 +0900

    [#156]test: AttributeExpr 기능 구현

--- app/visualize/analysis/stmt/parser/expr/parser/attribute_expr.py
+++ app/visualize/analysis/stmt/parser/expr/parser/attribute_expr.py
@@ -1,6 +1,23 @@
+from app.visualize.analysis.stmt.parser.expr.models.expr_obj import ExprObj, AttributeObj
+
+
 class AttributeExpr:
 
     @staticmethod
-    def parse(target, attr, args):
-        # Todo. AttributeExpr.parse() 메소드 구현
-        pass
+    def parse(target_obj: ExprObj, attr_name: str, arg_objs: list[ExprObj, ...]):
+        value = AttributeExpr._get_value(target_obj, attr_name, arg_objs)
+        expressions = AttributeExpr._create_expressions(target_obj, value)
+
+        return AttributeObj(value=value, expressions=expressions)
+
+    @staticmethod
+    def _get_value(target_obj: ExprObj, attr_name: str, arg_objs: list[ExprObj, ...]):
+        target_value = target_obj.value
+
+        getattr(target_value, attr_name)(*[arg.value for arg in arg_objs])
+
+        return target_value
+
+    @staticmethod
+    def _create_expressions(target_obj: ExprObj, value):
+        return tuple([target_obj.expressions[-1], str(value)])
