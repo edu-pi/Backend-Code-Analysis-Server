ab8f657580db05864656d5559b123a4a4a9a70fc
GS_song98 <20003204@sju.ac.kr>
2024-07-14 19:24:11 +0900

    [#156]test: AttributeExpr 테스트 케이스 추가

--- tests/visualize/analysis/stmt/parser/expr/parser/test_attribute_expr.py
+++ tests/visualize/analysis/stmt/parser/expr/parser/test_attribute_expr.py
@@ -1,23 +1,29 @@
 import pytest
 
-from app.visualize.analysis.stmt.parser.expr.models.expr_obj import NameObj, ConstantObj, AttributeObj
+from app.visualize.analysis.stmt.parser.expr.models.expr_obj import NameObj, ConstantObj, AttributeObj, ExprObj, ListObj
 from app.visualize.analysis.stmt.parser.expr.parser.attribute_expr import AttributeExpr
 
 
 @pytest.mark.parametrize(
-    "target, attr, args, expected",
+    "target_obj, attr_name, arg_objs, expected",
     [
         pytest.param(
-            NameObj(value="a", expressions=("a", "[1, 2, 3, 4, 5]")),
+            NameObj(value=[1, 2, 3, 4, 5], expressions=("a", "[1, 2, 3, 4, 5]")),
             "append",
             [ConstantObj(value=10, expressions=("10",))],
-            AttributeObj(value="[1, 2, 3, 4, 5, 10]", expressions=("[1, 2, 3, 4, 5]", "[1, 2, 3, 4, 5, 10]")),
+            AttributeObj(value=[1, 2, 3, 4, 5, 10], expressions=("[1, 2, 3, 4, 5]", "[1, 2, 3, 4, 5, 10]")),
             id="a.append(10): success case",
         ),
-        # Todo. 테스트 케이스 추가
+        pytest.param(
+            ListObj(value=[1, 2, 3, 4, 5], expressions=("[1, 2, 3, 4, 5]",)),
+            "append",
+            [ConstantObj(value=10, expressions=("10",))],
+            AttributeObj(value=[1, 2, 3, 4, 5, 10], expressions=("[1, 2, 3, 4, 5]", "[1, 2, 3, 4, 5, 10]")),
+            id="[1, 2, 3, 4, 5].append(10): success case",
+        ),
     ],
 )
-def test_parse_append_case(target, attr, args, expected):
-    result = AttributeExpr.parse(target, attr, args)
+def test_parse_append_case(target_obj: ExprObj, attr_name: str, arg_objs: list[ExprObj, ...], expected):
+    result = AttributeExpr.parse(target_obj, attr_name, arg_objs)
 
     assert result == expected
