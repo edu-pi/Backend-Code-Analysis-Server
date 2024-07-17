968f90b0290cdbad2287d9780fc68dbdd8421c74
UJeans <jean207@naver.com>
2024-07-17 15:42:14 +0900

    [#151]delete: 이전 range 테스트 삭제

--- tests/visualize/analysis/stmt/parser/test_for_stmt.py
+++ tests/visualize/analysis/stmt/parser/test_for_stmt.py
@@ -37,38 +37,6 @@ def test__get_target_name_fail(elem_container, target):
         ForStmt._get_target_name(target, elem_container)
 
 
-# TODO 함수마다 parser를 추가하는 방향 고민
-@pytest.mark.parametrize(
-    "code, expect",
-    [
-        (
-            """for i in range(3): \n    pass""",
-            ExprObj(
-                type="range",
-                value={"end": "3", "start": "0", "step": "1"},
-                expressions=[{"end": "3", "start": "0", "step": "1"}],
-            ),
-        )
-    ],
-)
-@pytest.mark.skip
-def test_get_condition_obj(create_ast, elem_container, code, expect):
-    # iter 노드를 받아서 range 함수의 파라미터를 반환하는지 테스트
-    iter_node = create_ast(code).iter
-
-    with patch.object(
-        ExprTraveler,
-        "call_travel",
-        return_value=ExprObj(
-            type="range",
-            value={"end": "3", "start": "0", "step": "1"},
-            expressions=[{"end": "3", "start": "0", "step": "1"}],
-        ),
-    ):
-        actual = ForStmt._get_condition_obj(iter_node, elem_container)
-        assert actual == expect
-
-
 @pytest.mark.parametrize(
     "body_steps",
     [
