61c3142ce49c8c59387b4815f8369443536110bc
GS_song98 <20003204@sju.ac.kr>
2024-08-02 14:42:58 +0900

    [#187]test: StmtTraveler _while_travel 테스트 코드 작성

--- app/visualize/analysis/stmt/parser/while_stmt.py
+++ app/visualize/analysis/stmt/parser/while_stmt.py
@@ -1,7 +1,6 @@
 import ast
 from dataclasses import replace
 
-from app.visualize.analysis.stmt.models.while_stmt_obj import WhileStmtObj
 from app.visualize.analysis.stmt.parser.expr.expr_traveler import ExprTraveler
 from app.visualize.container.element_container import ElementContainer
 
@@ -12,13 +11,6 @@ class WhileStmt:
     def parse_condition(test_node: ast.expr, elem_container: ElementContainer):
         test_obj = ExprTraveler.travel(test_node, elem_container)
         return WhileStmt.add_last_bool_expression(test_obj)
-
-    @staticmethod
-    def parse(call_id, while_steps):
-        return WhileStmtObj(
-            id=call_id,
-            while_cycles=while_steps,
-        )
 
     # ast.if와 ast.while에서 조건절에 사용할 경우 마지막 표현식이 bool이 아니라면 bool 표현을 추가하는
     # ex) "" -> False, "hello" -> True
