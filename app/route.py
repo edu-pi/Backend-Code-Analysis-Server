2adbf4af8aaf527985771227ed476ee558c2143f
GS_song98 <20003204@sju.ac.kr>
2024-08-08 17:48:41 +0900

    Revert "[#207]feat: py-eureka-client 설정"
    This reverts commit 273134a60cfc677a0bd479ca84da8f612c7c4b8b.

--- app/route.py
+++ app/route.py
@@ -1,31 +1,10 @@
-from contextlib import asynccontextmanager
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from py_eureka_client import eureka_client
 from pydantic import BaseModel
 
 from app.visualize.code_visualizer import CodeVisualizer
 
-
-async def init_eureka():
-    await eureka_client.init_async(
-        eureka_server="http://localhost:8761/eureka/", app_name="edupi-visualize", instance_port=8081, on_error=on_error
-    )
-
-
-def on_error(err_type: str, err: Exception):
-    print(err_type, err, sep=": ")
-
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    await init_eureka()
-    yield
-
-
-app = FastAPI(lifespan=lifespan)
-
+app = FastAPI()
 
 origins = [
     "*",
@@ -33,7 +12,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -44,12 +23,12 @@ class RequestCode(BaseModel):
     source_code: str
 
 
-@app.get("/edupi_visualize/v1/")
+@app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
-@app.post("/edupi_visualize/v1/python")
+@app.post("/v1/python")
 def read_root(request_code: RequestCode):
     # 코드 분석 인스턴스 생성
     code_analyzer = CodeVisualizer(request_code.source_code)
