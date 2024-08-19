3af92b2011f5f5a32e8781d71e5d325481c2bb61
GS_song98 <20003204@sju.ac.kr>
2024-08-19 19:44:05 +0900

    [#193]feat: 새로운 변수가 아닌 기존에 있던 리스트 내부의 값을 바꾸는 경우 추가

--- app/visualize/container/element_container.py
+++ app/visualize/container/element_container.py
@@ -18,8 +18,32 @@ class ElementContainer:
                 self.element_dict[name[i]] = value[i]
             return
 
+        # 할당해야 하는 target이 리스트 의 특정 인덱스인 경우
+        if "[" in name and "]" in name:
+            self._set_subscript_target(name, value)
+
         self.element_dict[name] = value
         return
 
     def get_element_dict(self):
         return self.element_dict
+
+    def _set_subscript_target(self, name, value):
+        # a[0:1]에서 a 추출
+        list_name = name.split("[")[0]
+        # a[0:1]에서 0:1 부분 추출
+        sliced_index = name.split("[")[1].split("]")[0]
+        # 0:1 -> [0, 1]
+        slice_list = [int(x) for x in sliced_index.split(":")]
+
+        # 해당 list를 찾아온다.
+        find_list = self.element_dict[list_name]
+
+        # start, end, step 인 경우
+        if len(slice_list) > 1:
+            find_list[slice(*slice_list)] = value
+            return
+
+        # 값 하나만 바꾸는 경우
+        find_list[int(slice_list[0])] = value
+        return
