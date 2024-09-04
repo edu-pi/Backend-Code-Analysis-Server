fec56dea7ab1ef75b794b130d65d36ab5cd5876f
UJeans <jean207@naver.com>
2024-09-04 14:47:51 +0900

    [#218]feat: ast.List 타입 테스트

--- tests/visualize/generator/converter/test_for_header_converter.py
+++ tests/visualize/generator/converter/test_for_header_converter.py
@@ -1,6 +1,6 @@
 import pytest
 
-from app.visualize.analysis.stmt.parser.expr.models.expr_obj import ExprObj
+from app.visualize.analysis.stmt.parser.expr.models.expr_obj import ExprObj, ListObj
 from app.visualize.analysis.stmt.parser.expr.models.expr_type import ExprType
 from app.visualize.generator.converter.for_header_converter import ForHeaderConvertor
 from app.visualize.generator.models.for_viz import ForConditionViz
@@ -20,3 +20,25 @@ def test__get_name_condition(iter_value, expected):
     actual = ForHeaderConvertor._get_list_condition(mock_target_name, iter_obj)
 
     assert actual == expected
+
+
+def test__get_condition(mocker):
+    target = "i"
+    iter_Obj = ListObj(value=[1, 2, 3, 4], expressions=("[1,2,3,4]",))
+    mock_get_condition = mocker.patch.object(ForHeaderConvertor, "_get_list_condition")
+
+    ForHeaderConvertor._get_condition(target, iter_Obj)
+
+    mock_get_condition.assert_called_once_with(target, iter_Obj)
+
+
+def test__get_list_condition():
+    iter_Obj = ListObj(value=[0, 1, 2, 3, 4], expressions=("[0,1,2,3,4]",))
+
+    actual = ForHeaderConvertor._get_list_condition("i", iter_Obj)
+
+    assert isinstance(actual, ForConditionViz)
+    assert actual.target == "i"
+    assert actual.start == "0"
+    assert actual.end == "4"
+    assert actual.step == "1"
