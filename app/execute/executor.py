eb73af398459084bb8b0891c15bb2e622036cc60
UJeans <jean207@naver.com>
2024-10-02 15:47:25 +0900

    [#228]feat: 코드 실행 결과 받기

--- app/execute/executor.py
+++ app/execute/executor.py
@@ -1,4 +1,4 @@
-from RestrictedPython import compile_restricted, safe_globals, limited_builtins, PrintCollector
+from RestrictedPython import compile_restricted, safe_globals, PrintCollector
 from RestrictedPython.Guards import guarded_setattr
 
 from app.execute.exception.code_execute_error import CodeExecuteError
@@ -9,26 +9,44 @@ class Executor:
     def __init__(self, source_code: str):
         self.code = source_code
 
+    def _get_iter(self, obj):
+        """객체가 반복 가능하면 반복자를 반환."""
+        if hasattr(obj, "__iter__"):
+            return iter(obj)
+
+        raise TypeError(f"{obj} is not iterable")
+
     def run(self):
-        # 제한된 환경에서 컴파일
-        byte_code = compile_restricted(self.code, filename="<string>", mode="exec")
-
-        # 실행 환경 (globals와 locals을 제공)
-        restricted_globals = safe_globals.copy()
-        restricted_globals.update(
-            {
-                "__builtins__": limited_builtins,  # 제한된 built-ins 사용
-                "_print_": PrintCollector,  # print를 위한 PrintCollector 사용
-                "_getattr_": guarded_setattr,
-            }
-        )
-        restricted_locals = {}
-
         try:
-            # 코드 실행
-            exec(byte_code, restricted_globals, restricted_locals)
+            # 제한된 환경에서 컴파일
+            byte_code = compile_restricted(self.code, filename="<string>", mode="exec")
 
-            # 실행 결과를 locals에서 확인
-            print(restricted_locals)  # 8 출력
+            # 실행 환경 (globals와 locals을 제공)
+            restricted_globals = safe_globals.copy()
+            restricted_globals.update(
+                {
+                    "__builtins__": {
+                        "range": range,
+                        "list": list,
+                        "tuple": tuple,
+                        "type": type,
+                        "map": map,
+                        "_getattr_": getattr,
+                    },  # 제한된 built-ins 사용
+                    "_print_": PrintCollector,
+                    "_setattr_": guarded_setattr,
+                }
+            )
+            restricted_locals = {}
+            exec(byte_code, restricted_globals, restricted_locals)
+            return self._get_print_result(restricted_locals)
+
         except Exception as e:
             raise CodeExecuteError(ErrorEnum.CODE_EXEC_ERROR, e.args)
+
+    def _get_print_result(self, restricted_locals):
+        """_print 변수가 존재할 경우, 그 결과 반환."""
+        if "_print" in restricted_locals:
+            if isinstance(restricted_locals["_print"], PrintCollector):
+                return "".join(restricted_locals["_print"].txt)  # 리스트 요소를 합쳐 하나의 문자열로
+        return ""  # _print가 없거나 잘못된 형식일 경우 빈 문자열 반환
--- app/route.py
+++ app/route.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from starlette.responses import JSONResponse
 
 from app.execute.executor import Executor
 from app.visualize.code_visualizer import CodeVisualizer
 from app.web import exception_handler
+from app.web.models.success_reponse import SuccessResponse
 
 app = FastAPI()
 
@@ -32,5 +34,11 @@ def read_root(request_code: RequestCode):
 @app.post("/edupi-visualize/v1/run")
 def run(request_code: RequestCode):
     # 코드 분석 인스턴스 생성
-    executor = Executor(request_code.source_code)
-    executor.run()
+    executor = Executor(
+        request_code.source_code,
+    )
+    result = executor.run()
+
+    success_response = SuccessResponse(detail="success run", result={"output": result})
+
+    return JSONResponse(status_code=200, content=success_response.to_dict())
