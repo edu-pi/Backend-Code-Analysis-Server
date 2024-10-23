da883b938d6334aeee6ef573091bf01054217fee
GS_song98 <20003204@sju.ac.kr>
2024-10-23 17:47:46 +0900

    [#241]feat: Return의 value가 없을 때 추가

--- app/visualize/analysis/stmt/parser/return_stmt.py
+++ app/visualize/analysis/stmt/parser/return_stmt.py
@@ -8,5 +8,7 @@ from app.visualize.container.element_container import ElementContainer
 class ReturnStmt:
     @staticmethod
     def parse(node: ast.Return, elem_container: ElementContainer) -> ReturnStmtObj:
+        if node.value is None:
+            return ReturnStmtObj(id=node.lineno, value=None, expr=("",))
         return_value_obj = ExprTraveler.travel(node.value, elem_container)
         return ReturnStmtObj(id=node.lineno, value=return_value_obj.value, expr=return_value_obj.expressions)
